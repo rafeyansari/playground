309fd91abab7990af3804493f2727f13f06674f7
rafeyansari <rafey.a@gmail.com>
2015-02-01 23:06:20 -0500

    adding angular testing, started work on topcorner page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
+(function() {
+  var app = angular.module('slideShow', []);
 
-});
+  app.controller('SlideshowController', function() {
+    this.slide = slide;
+  });
+
+  var slide = {
+    name: 'Slide 1',
+    description: 'Slide Description',
+    dimensions: 'Slide Dimensions'
+  };
+})();
